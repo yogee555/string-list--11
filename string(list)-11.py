51994637df91529e41fcfbc446cbec0698fa71c8
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-04-05 11:21:06 +0530

    Add files via upload

---
+++ string(list)-11.py
@@ -0,0 +1,5 @@
+# Appending a new element
+L1 = [1,"a",2,"b",3,"c"]   # store value in L1
+print(L1)                  # print L1 store
+L1.append("poorna")        # Add extra value in list
+print(L1)                  # Execute the add value in list
